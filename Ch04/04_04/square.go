4f274ddd66674eb5d2a580af8592f691e1e07bc4
JiajingFang <krisssfong@gmail.com>
2023-09-13 17:02:02 +0200

    square

--- Ch04/04_04/square.go
+++ Ch04/04_04/square.go
@@ -7,24 +7,41 @@ import (
 
 // Square is a square
 type Square struct {
-	// TODO
+	x      int
+	y      int
+	length int
 }
 
 // NewSquare returns a new square
 func NewSquare(x int, y int, length int) (*Square, error) {
-	// TODO
-	return nil, nil
+	if x <= 0 {
+		return nil, fmt.Errorf("x must be larger than 0")
+	}
+	if y <= 0 {
+		return nil, fmt.Errorf("y must be larger than 0")
+	}
+	if length <= 0 {
+		return nil, fmt.Errorf("length must be larger than 0")
+	}
+	s := Square{
+		x:      x,
+		y:      y,
+		length: length,
+	}
+
+	return &s, nil
 }
 
 // Move moves the square
 func (s *Square) Move(dx int, dy int) {
 	// TODO
+
 }
 
 // Area returns the square are
 func (s *Square) Area() int {
 	// TODO
-	return 0
+	return s.x * s.y * s.length
 }
 
 func main() {
